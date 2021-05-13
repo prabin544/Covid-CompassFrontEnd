07644314546fa93ba3f21c0c8f882c95b7c3b598
mrloulass <louislassegue@gmail.com>
2021-05-12 21:38:04 -0700

    About us 2nd try

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -12,7 +12,7 @@ class Navigation extends React.Component {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link href="#About Us">About Us</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#pricin">Pricin</Nav.Link>
             <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
